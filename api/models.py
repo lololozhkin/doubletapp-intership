596c2100a1979e39006224362ed975bfa682bf6b
lololozhkin <alexand3r.lozhkin@gmail.com>
2021-04-18 18:54:01 +0500

    plural categories added in admin pannel

--- api/models.py
+++ api/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=255)
     icon = models.URLField()
 
